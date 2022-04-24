272a9705736896633cc6283d0dd3534d09aa949f
Martyna <m.smolarek19@gmail.com>
2022-04-24 22:00:57 +0200

    added buttons

---
+++ src/components/atoms/button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Button = ({variant, content}) => {
+
+const buttonClasses = {}
+
+  return (
+    <>
+    <button className=''>{content}</button>
+    </>
+  ) 
+}
+
+export default Button
