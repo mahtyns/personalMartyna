6c71053cf1db2ddf8413e7094f11913827353023
Martyna <m.smolarek19@gmail.com>
2022-04-24 22:04:50 +0200

    problems netlify

--- src/components/atoms/mainLogo/MainLogo.jsx
+++ src/components/atoms/mainLogo/MainLogo.jsx
@@ -4,7 +4,7 @@ import './mainLogoStyles.css'
 const MainLogo = () => {
   return (
     <div className='mainLogoContainer'>
-      <img src="https://i.ibb.co/Lx0F3Z8/universe.png" alt='ToBeChanged-Logo Lab'/>
+      <img src="" alt='ToBeChanged-Logo Lab'/>
     </div>
   )
 }
